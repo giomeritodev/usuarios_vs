8d8e7aee22c5c1bfae629efbf57611ef49c81491
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-12-01 08:08:21 -0300

    Acessando os inputs com forEach

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 let name = document.querySelector("#exampleInputName");
 let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 let birth = document.querySelector("#exampleInputBirth");
@@ -6,3 +7,10 @@ let email = document.querySelector("#exampleInputEmail");
 let password = document.querySelector("#exampleInputPassword");
 let file = document.querySelector("#exampleInputFile");
 let admin = document.querySelector("#exampleInputAdmin");
+*/
+
+let fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach((field, index) => {
+    console.log(field.name, index);
+});
