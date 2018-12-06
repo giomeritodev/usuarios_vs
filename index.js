d34a754b6a5b741fd66acf32e9152e3c8a39c7c8
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-12-05 21:47:57 -0300

    Trabalhando com eventos no JavaScript

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-/*
-let name = document.querySelector("#exampleInputName");
-let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-let birth = document.querySelector("#exampleInputBirth");
-let country = document.querySelector("#exampleInputCountry");
-let email = document.querySelector("#exampleInputEmail");
-let password = document.querySelector("#exampleInputPassword");
-let file = document.querySelector("#exampleInputFile");
-let admin = document.querySelector("#exampleInputAdmin");
-*/
-
 let fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
-fields.forEach((field, index) => {
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+    event.preventDefault();
     
-    if(field.name == "gender"){
-        if(field.checked){
+    fields.forEach((field, index) => {
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
             user[field.name] = field.value;
         }
-    }else{
-        user[field.name] = field.value;
-    }   
-    
-});
 
-console.log(user);
+    });
+
+    console.log(user);
+});
