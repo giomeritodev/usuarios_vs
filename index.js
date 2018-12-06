e54bf2753e5f843eff3693dfae9eccb30beb865f
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-12-06 12:46:23 -0300

    POO - Classes e MVC

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+//Metodo para listar todos os campos do formulário pelo "id"
 let fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
 //Função para salvar os dados na tabela
 function addLine(dataUser) {
 
-    let tr = document.createElement("tr");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -19,8 +18,6 @@ function addLine(dataUser) {
             </td>
         </tr>
     `;
-
-    document.getElementById("table-users").appendChild(tr);
 }
 
 //Comando para ler os campos do html
@@ -39,5 +36,16 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    addLine(user);
+    let objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+    addLine(objectUser);    
 });
