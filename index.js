9142052b36796d8971700b7dba9ff4cb56103287
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-12-05 22:36:05 -0300

    Trabalhando com funcoes

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 let fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
+//Função para salvar os dados na tabela
+function addLine(dataUser) {
 
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+}
+
+//Comando para ler os campos do html
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     fields.forEach((field, index) => {
 
         if (field.name == "gender") {
@@ -17,5 +39,5 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    console.log(user);
+    addLine(user);
 });
