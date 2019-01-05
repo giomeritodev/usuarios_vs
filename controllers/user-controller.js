0e13db63d374489459f45a1ec2053df81ddffcd1
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2019-01-04 21:43:23 -0300

    Armazenando dados na sessao com sessionStorage

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,6 +9,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
 
     }//Fim do construct
 
@@ -88,6 +89,8 @@ class UserController {
             this.getPhoto(this.formEl).then(
                 (content) => {
                     values.photo = content;
+
+                    this.insert(dataUser);
 
                     this.addLine(values);
 
@@ -174,6 +177,40 @@ class UserController {
             user.admin
         );
     }//Fechando o Método getVelues()
+
+    getUsersStorage(){
+        let users = [];
+
+        if(sessionStorage.getItem("users")){
+            users = JSON.parse(sessionStorage.getItem("users"));
+        }
+
+        return users;
+    }//Fechamento do getUsersStorage()
+
+    selectAll(){
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+        });
+
+    }//Fechamento selectAll()
+
+    insert(data){
+
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        sessionStorage.setItem("users", JSON.stringify(users));
+
+    }//Fechamento do insert
 
     //Metodo para salvar os dados na tabela
     addLine(dataUser) {
